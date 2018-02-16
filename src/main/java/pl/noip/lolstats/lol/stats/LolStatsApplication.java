1702b060d98efffe039cb2d88d21285a977edcd1
Piotr Iwaniuk <piekoszek@gmail.com>
2018-02-16 21:00:14 +0100

    pr test

--- src/main/java/pl/noip/lolstats/lol/stats/LolStatsApplication.java
+++ src/main/java/pl/noip/lolstats/lol/stats/LolStatsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LolStatsApplication {
-//pr test
+//pr test 
 	public static void main(String[] args) {
 		SpringApplication.run(LolStatsApplication.class, args);
 	}
