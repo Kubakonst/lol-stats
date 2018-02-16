f7c96b67e247b9f7d68c1ec4084651cb50016284
Piotr Iwaniuk <piekoszek@gmail.com>
2018-02-16 20:58:20 +0100

    develop branch

--- src/main/java/pl/noip/lolstats/lol/stats/LolStatsApplication.java
+++ src/main/java/pl/noip/lolstats/lol/stats/LolStatsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LolStatsApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(LolStatsApplication.class, args);
 	}
