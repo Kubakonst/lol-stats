792456b341d1bd03bf0bba4a194aec48a9cade9e
Piotr Iwaniuk <piekoszek@gmail.com>
2018-02-16 20:13:43 +0100

    Initial commit

---
+++ src/main/java/pl/noip/lolstats/lol/stats/LolStatsApplication.java
@@ -0,0 +1,12 @@
+package pl.noip.lolstats.lol.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolStatsApplication.class, args);
+	}
+}
