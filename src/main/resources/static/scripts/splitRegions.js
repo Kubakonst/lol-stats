cf30ee55ae76df5a0b54a97f84b7a93d6e5d0b63
Kubakonst <chelsea11>
2018-05-16 14:25:20 +0200

    ceated some js

--- src/main/resources/static/scripts/splitRegions.js
+++ src/main/resources/static/scripts/splitRegions.js
@@ -4,22 +4,32 @@ var sumNameElement = document.getElementById("summonerNameInput");
 
     if (sumNameElement.value.length > 0){
                             riotNameRestCall(sumNameElement.value,
-                                            regionSave(json.region)
-                                            maleUL()
+                                            regionSave(json.region),
                                          (e) => document.getElementById("error").innerHTML = e.error);
                             }
 }
 
-function makeUL(){
+function makeUl(){
 
 var regions = regionLoad();
 
-    for(var i = 0; j < regions[i].length; i++)
+    for(var i = 0; i < regions[i].length; i++)
     {
        var btn = document.createElement("BUTTON");
-             var reg = document.createTextNode(key[j]);
+             var reg = document.createTextNode(regions[i]);
              btn.appendChild(t);
        			document.body.appendChild(btn);
+       			btn.onclick = sendReg;
     }
 
+}
+
+function sendReg(){
+
+var sumNameElement = document.getElementById("summonerNameInput");
+
+       nameRestCall(sumNameElement.value,
+                            reg,
+                            () => window.location.href = "/registration-step-1.html",
+                            (e) => document.getElementById("error").innerHTML = e.error);
 }
