0b525015566e40580d2e6bdea1e9998e738cd7e9
Kubakonst <chelsea11>
2018-07-05 12:13:04 +0200

    loading animation during region searching

--- src/main/resources/static/scripts/splitRegions.js
+++ src/main/resources/static/scripts/splitRegions.js
@@ -1,10 +1,8 @@
 function onRegions(){
-console.log("dasdasas");
-//setInterval(function(){ alert("Hello"); }, 1000);
+
 var loading = document.createElement("img");
 document.getElementById('regions').appendChild(loading);
 loading.src = "img/loadgif.gif";
-console.log(loading);
 
 var sumNameElement = document.getElementById("summonerNameInput");
 var name = document.getElementById("summonerName");
@@ -31,9 +29,10 @@ function makeList(regions){
             listElement.appendChild(listItem);
 listItem.onclick = () => { sendReg(listItem.innerHTML)}
 var region = listItem.innerHTML;
+loading.src = " ";
 
         }
-//listItem.onclick = sendReg(regions[i]);
+
 }
 
 }
