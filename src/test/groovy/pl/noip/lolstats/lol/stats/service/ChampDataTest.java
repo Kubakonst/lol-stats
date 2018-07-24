12acb5e72b7c38f9b087d785f32c21fda336e4f0
Kubakonst <chelsea11>
2018-07-24 15:42:37 +0200

    json uploaded

---
+++ src/test/groovy/pl/noip/lolstats/lol/stats/service/ChampDataTest.java
@@ -0,0 +1,26 @@
+package pl.noip.lolstats.lol.stats.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class ChampDataTest {
+
+    @Autowired
+    ChampData champData;
+
+    @Test
+    public void getjsondata() {
+        assertNotNull(champData.champJsonData());
+        System.out.println(champData.champJsonData());
+    }
+
+}
